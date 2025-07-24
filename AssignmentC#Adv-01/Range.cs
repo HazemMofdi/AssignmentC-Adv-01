0d96994fe6e929196f7f1baf96f3f52284af5c72
Hazem Mofdi <hazemmofdi11@gmail.com>
2025-07-24 14:33:05 +0300

    Add project files.

---
+++ AssignmentC#Adv-01/Range.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentC_Adv_01
+{
+    public class Range<T> where T : IComparable<T>, INumber<T>
+    {
+        
+        public T Min { get; set; }
+        public T Max { get; set; }
+        public Range(T _Max, T _Min)
+        {
+            Min = _Min;
+            Max = _Max;
+        }
+
+        public bool IsInRange(T Val)
+        {
+            return Min.CompareTo(Val) <=0 && Max.CompareTo(Val) >= 0;
+        }
+
+        
+        public int Length(T max, T min)
+        {
+            return int.CreateChecked(max - min);
+        }
+
+    }
+}
