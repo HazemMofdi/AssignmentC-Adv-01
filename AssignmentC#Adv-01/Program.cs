753e245b6e01166b533f6e7f96ad54f80c8626a7
Hazem Mofdi <hazemmofdi11@gmail.com>
2025-07-29 19:28:21 +0300

    Final Version

--- AssignmentC#Adv-01/Program.cs
+++ AssignmentC#Adv-01/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
+            RangeClass<int> range = new RangeClass<int>(2,10);
+            int[] arr = { 8, 3, 2, 6, 5, 4, 7, 1 };
 
+
+            foreach (int i in arr)
+            {
+                Console.WriteLine(i);
+            }
+            range.BubbleSort(arr);
+            Console.WriteLine("----After Sorting----");
+            foreach (int i in arr)
+            {
+                Console.WriteLine(i);
+            }
+
+            
         }
     }
 }
---
+++ AssignmentC#Adv-01/RangeClass.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentC_Adv_01
+{
+    public class RangeClass<T> where T : INumber<T>
+    {
+        
+        public T Min { get; set; }
+        public T Max { get; set; }
+        public RangeClass()
+        {
+            
+        }
+        public RangeClass(T _Max, T _Min)
+        {
+            Min = _Min;
+            Max = _Max;
+        }
+
+        public T[] BubbleSort(T[] arr)
+        {
+            bool swapped;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                swapped = false;
+                for(int j = 0 ; j < arr.Length - i - 1; j++)
+                {
+                    if(arr[j].CompareTo(arr[j + 1]) > 0)
+                    {
+                        T tmp = arr[j]; 
+                        arr[j] = arr[j + 1]; 
+                        arr[j + 1] = tmp;
+                        swapped = true;
+                    }
+                }
+                if (!swapped)
+                    break;
+            }
+            return arr;
+        }
+
+        public bool IsInRange(T Val)
+        {
+            return Min.CompareTo(Val) <= 0 && Max.CompareTo(Val) >= 0;
+        }
+
+        public T Length(T max, T min)
+        {
+            return max - min;
+        }
+
+    }
+}
